cf6d5fe8141f10aaba6f0dbc5230d00154db2117
BrenoBrbz <brenobarboza4533@gmail.com>
2025-06-06 16:38:30 -0300

    movement, animation and camera

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     Rigidbody2D myRigidbody;
     public float speed = 5f;
+    public float forceJump = 5f;
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.7f;
+    public Ease ease = Ease.OutBack;
+
+    public float animationduration = 0.3f;
 
     void Start()
     {
@@ -11,6 +18,12 @@ public class Player : MonoBehaviour
     }
 
     void Update()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void Walk()
     {
         if (Input.GetKey(KeyCode.A))
         {
@@ -22,8 +35,25 @@ public class Player : MonoBehaviour
         }
         else
         {
-            // Mantém a velocidade horizontal zero quando não pressiona A ou D
             myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y);
         }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, forceJump);
+            transform.localScale = Vector3.one;
+
+            DOTween.Kill(myRigidbody.transform);
+
+            ScaleJump();
+        }
+    }
+    private void ScaleJump()
+    {
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationduration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationduration).SetLoops(2, LoopType.Yoyo).SetEase(ease);      
     }
 }
